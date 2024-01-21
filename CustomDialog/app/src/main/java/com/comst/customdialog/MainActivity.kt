22c0c6f318c77a368cafecb8dbc8762412fcdcbe
comst19 <comst19@naver.com>
2024-01-21 23:41:19 +0900

    Custom Dialog

---
+++ CustomDialog/app/src/main/java/com/comst/customdialog/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.comst.customdialog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.comst.customdialog.ui.theme.CustomDialogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomDialogTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CustomDialog()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomDialog() {
+    var openDialog by remember { mutableStateOf(false) }
+    var counter by remember { mutableStateOf(0) }
+    
+    Column{
+        Button(onClick = {
+            openDialog = true
+        }) {
+            Text(text = "다이얼로그 열기")
+        }
+        Text(text = "카운터: $counter")
+    }
+    
+    if (openDialog){
+        Dialog(onDismissRequest = {
+            // 단계 1: 디스미스 처리를 한다
+            openDialog = false
+        }) {
+            Surface{
+                // 단계 2: 컬럼을 만들고 설명을 적어보자
+                Column(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "버튼을 클릭해주세요.\n* +1을 누르면 값이 증가됩니다.\n* -1을 누르면 값이 감소합니다.")
+                    Row(modifier = Modifier.align(Alignment.End)){
+                        Button(onClick = {
+                            openDialog = false
+                        }) {
+                            Text(text = "취소")
+                        }
+                       Button(onClick = {
+                           openDialog = false
+                            counter++
+                       }) {
+                           Text(text = "+1")
+                       }
+                        Button(onClick = {
+                            openDialog = false
+                            counter--
+                        }) {
+                            Text(text = "-1")
+                        }
+
+                    }
+                }
+                // 단계3 : 컬럼 안에 로우를 만들어 수평 방향으로 버튼을 배열한다.
+                // 버튼은 +1, -1, 취소로 구성한다.
+                
+                // +1은 counter를 증가시키고 -1은 감소, 취소는 다이얼로그를 닫는다.
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CustomDialogTheme {
+        CustomDialog()
+    }
+}
