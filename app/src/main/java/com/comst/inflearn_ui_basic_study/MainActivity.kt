cf7d6f0a648ae35cadfd9a7499c51310c06715b8
comst19 <comst19@naver.com>
2024-02-09 07:06:13 +0900

    인프런 : Column/Spacer/Divider

--- app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
+++ app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
@@ -3,8 +3,12 @@ package com.comst.inflearn_ui_basic_study
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,7 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.comst.inflearn_ui_basic_study.ui.theme.Inflearn_ui_basic_studyTheme
 
@@ -24,29 +30,52 @@ class MainActivity : ComponentActivity() {
         setContent {
             Inflearn_ui_basic_studyTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    SimpleCounterBtn()
-                }
+                ColumnTest2()
             }
         }
     }
 }
 
 @Composable
-fun SimpleCounterBtn(){
-    var count by remember { mutableStateOf(0) }
+fun ColumnTest1(){
+    Text(
+        text = "안녕하세요1",
+        fontSize = 30.sp
+    )
+    Text(
+        text = "반갑습니다1",
+        fontSize = 30.sp,
+        modifier = Modifier.padding(top = 50.dp)
+    )
+    Text(
+        text = "반갑습니다2",
+        fontSize = 30.sp,
+        modifier = Modifier.padding(top = 100.dp)
+    )
+}
 
-    Button(onClick = {
-        count++
-    },
-        modifier = Modifier.fillMaxSize()
+@Composable
+fun ColumnTest2(){
+    Column(
+        modifier = Modifier.padding(30.dp)
     ) {
         Text(
-            text = "Count : $count",
-            fontSize = 50.sp
+            text = "안녕하세요1",
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.padding(30.dp))
+        Divider(
+            thickness = 4.dp,
+            color = Color.Blue
+        )
+        Spacer(modifier = Modifier.padding(30.dp))
+        Text(
+            text = "안녕하세요2",
+            fontSize = 30.sp
+        )
+        Text(
+            text = "안녕하세요3",
+            fontSize = 30.sp
         )
     }
 }
@@ -56,6 +85,6 @@ fun SimpleCounterBtn(){
 @Composable
 fun GreetingPreview() {
     Inflearn_ui_basic_studyTheme {
-        SimpleCounterBtn()
+        ColumnTest2()
     }
 }
