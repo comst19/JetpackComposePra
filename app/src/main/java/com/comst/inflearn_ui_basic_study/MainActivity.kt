e91d112a09ec7d5db4a09463b8a9622f1b285f89
comst19 <comst19@naver.com>
2024-02-08 23:56:09 +0900

    인프런 : remember

--- app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
+++ app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
@@ -1,28 +1,20 @@
 package com.comst.inflearn_ui_basic_study
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.comst.inflearn_ui_basic_study.ui.theme.Inflearn_ui_basic_studyTheme
 
@@ -36,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyBtn()
+                    SimpleCounterBtn()
                 }
             }
         }
@@ -44,48 +36,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyBtn() {
+fun SimpleCounterBtn(){
+    var count by remember { mutableStateOf(0) }
 
-    val context = LocalContext.current
-    Button(
-        onClick = {
-            Log.d("Main", "onClick")
-            Toast.makeText(context, "클릭완료", Toast.LENGTH_SHORT).show()
-        },
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Yellow,
-            contentColor = Color.Blue
-        ),
-        modifier = Modifier
-            .width(200.dp)
-            .height(300.dp)
+    Button(onClick = {
+        count++
+    },
+        modifier = Modifier.fillMaxSize()
     ) {
         Text(
-            text = "버튼버튼 버튼입니다. 버튼버튼버튼",
-            lineHeight = 50.sp,
-            fontSize = 30.sp,
-            color = Color.Red
+            text = "Count : $count",
+            fontSize = 50.sp
         )
     }
 }
 
-@Composable
-fun MyTextEx() {
-    Text(
-        text = "안녕하세요 텍스트 예제입니다.",
-        fontSize = 30.sp,
-        color = Color.Red,
-        fontStyle = FontStyle.Italic,
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(30.dp),
-        style = TextStyle(background = Color.Blue)
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Inflearn_ui_basic_studyTheme {
-        MyBtn()
+        SimpleCounterBtn()
     }
 }
