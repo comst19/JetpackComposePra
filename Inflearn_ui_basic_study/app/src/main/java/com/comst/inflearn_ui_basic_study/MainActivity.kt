09d39b850cfa3a8050d5b31078553062255a4bac
comst19 <comst19@naver.com>
2024-02-10 16:04:54 +0900

    인프런 : Box

--- Inflearn_ui_basic_study/app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
+++ Inflearn_ui_basic_study/app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -39,28 +42,107 @@ class MainActivity : ComponentActivity() {
         setContent {
             Inflearn_ui_basic_studyTheme {
                 // A surface container using the 'background' color from the theme
-                MyImageTest2()
+                BoxExample()
             }
         }
     }
 }
 
 @Composable
-fun MyImageTest1(){
-    Image(
-        painter = painterResource(id = R.drawable.insider),
-        contentDescription = "shop"
-    )
-}
+fun BoxExample() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Red)
+    ){
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Red)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color = Color.Blue)
+                    .padding(16.dp)
+                    .align(Alignment.TopStart)
+            ) {
+                Text(text = "왼쪽 위")
+            }
 
-@Composable
-fun MyImageTest2(){
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color = Color.Green)
+                    .padding(16.dp)
+                    .align(Alignment.TopCenter)
+            ) {
+                Text(text = "중앙 위")
+            }
 
-    AsyncImage(
-        model = "https://avatars.githubusercontent.com/u/69802523?v=4&size=150",
-        contentDescription = "깃허브 프로필",
-        modifier = Modifier.fillMaxSize()
-    )
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color = Color.Gray)
+                    .padding(16.dp)
+                    .align(Alignment.TopEnd)
+            ) {
+                Text(text = "오른쪽 위")
+            }
+
+            Button(
+                onClick = {  },
+                modifier = Modifier.align(Alignment.CenterStart)
+            ) {
+                Text(text = "중앙 왼쪽")
+            }
+
+            Button(
+                onClick = {  },
+                modifier = Modifier.align(Alignment.Center)
+            ) {
+                Text(text = "중앙")
+            }
+
+            Button(
+                onClick = {  },
+                modifier = Modifier.align(Alignment.CenterEnd)
+            ) {
+                Text(text = "중앙 오른쪽")
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color = Color.Blue)
+                    .padding(16.dp)
+                    .align(Alignment.BottomStart)
+            ) {
+                Text(text = "왼쪽 아래")
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color = Color.Green)
+                    .padding(16.dp)
+                    .align(Alignment.BottomCenter)
+            ) {
+                Text(text = "중앙 아래")
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(color = Color.Gray)
+                    .padding(16.dp)
+                    .align(Alignment.BottomEnd)
+            ) {
+                Text(text = "오른쪽 아래")
+            }
+
+        }
+    }
 }
 
 
@@ -68,6 +150,6 @@ fun MyImageTest2(){
 @Composable
 fun GreetingPreview() {
     Inflearn_ui_basic_studyTheme {
-        MyImageTest2()
+        BoxExample()
     }
 }
