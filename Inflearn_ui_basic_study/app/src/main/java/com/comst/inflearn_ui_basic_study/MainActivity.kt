4d26d02c668c410f0d4444b1f37b577017aa33f7
comst19 <comst19@naver.com>
2024-02-11 19:54:34 +0900

    인프런 : Column + Row

--- Inflearn_ui_basic_study/app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
+++ Inflearn_ui_basic_study/app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
@@ -3,8 +3,10 @@ package com.comst.inflearn_ui_basic_study
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -29,9 +32,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -44,7 +49,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Inflearn_ui_basic_studyTheme {
                 // A surface container using the 'background' color from the theme
-                RowTest()
+                ColumnRowTest2()
             }
         }
     }
@@ -52,35 +57,120 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun RowTest(){
+fun ColumnRowTest1() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+            .background(Color.Gray)
+    ) {
+        Text(
+            text = "안녕하세요",
+            color = Color.Blue,
+            fontSize = 20.sp
+        )
 
-    Row(
-        modifier = Modifier.fillMaxSize().background(Color.Gray),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "왼쪽")
+            Text(text = "중앙")
+            Text(text = "오른쪽")
+
+        }
+
         Text(
-            text = "Item1",
-            style = TextStyle(background = Color.Blue),
-            fontSize = 30.sp
+            text = "반갑습니다.",
+            color = Color.Red,
+            fontSize = 20.sp
         )
-        Text(
-            text = "Item2",
-            style = TextStyle(background = Color.Red),
-            fontSize = 30.sp
-        )
-        Text(
-            text = "Item3",
-            style = TextStyle(background = Color.Green),
-            fontSize = 30.sp
-        )
+
     }
 }
+
+@Composable
+fun ColumnRowTest2() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+            .background(Color.Cyan)
+            .border(
+                border = BorderStroke(5.dp, color = Color.Blue)
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier.padding(top = 20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.insider),
+                contentDescription = "인사이더 프로젝트 이미지",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(50.dp))
+            )
+        }
+
+        Text(
+            text = "컴쓰트",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 50.dp)
+        )
+
+        Text(
+            text = "Android Developer",
+            fontSize = 15.sp,
+            modifier = Modifier.padding(top = 10.dp)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "이메일",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "comst19@naver.com",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(10.dp),
+                color = Color.Blue
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = "연락처",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(10.dp)
+            )
+
+            Text(
+                text = "000-0000-0000",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(10.dp),
+                color = Color.Blue
+            )
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Inflearn_ui_basic_studyTheme {
-        RowTest()
+        ColumnRowTest2()
     }
 }
