4bb568df7db6dd99a3df6fdbfe8e833ad9ea1331
comst19 <comst19@naver.com>
2024-02-17 22:13:34 +0900

    인프런 : Retrofit - 2

--- Inflearn_ui_basic_study/app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
+++ Inflearn_ui_basic_study/app/src/main/java/com/comst/inflearn_ui_basic_study/MainActivity.kt
@@ -84,6 +84,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 // https://jsonplaceholder.typicode.com/posts/1
 class MainActivity : ComponentActivity() {
@@ -91,34 +92,73 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Inflearn_ui_basic_studyTheme {
-
-                val coroutineScope = rememberCoroutineScope()
-                val retrofitInstance = RetrofitInstance.getInstance().create(MyApi::class.java)
-
-                // A surface container using the 'background' color from the theme
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-                    Button(onClick = {
-                        coroutineScope.launch {
-                            val response = retrofitInstance.getPost1()
-                            Log.d("MM", response.body().toString())
-                        }
-                    }) {
-                        Text(text = "CALL API")
-                    }
-                }
+                InsertInputData()
             }
         }
     }
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertInputData() {
+
+    var inputNumber by remember { mutableStateOf("") }
+
+    var post by remember {
+        mutableStateOf<Post?>(null)
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = inputNumber,
+            onValueChange = { inputNumber = it },
+            label = { Text("Label") }
+        )
+
+        Button(onClick = {
+            val number = inputNumber.toIntOrNull()
+            if (number != null){
+                coroutineScope.launch {
+                    post = getPostData(number)
+                }
+            }
+        }) {
+            Text(text = "데이터 받아오기")
+        }
+        post?.let {
+
+            Text(text = "UserId : "+it.userId)
+            Text(text = "ID : "+it.id)
+            Text(text = "Title : "+it.title)
+            Text(text = "Body : "+it.body)
+        }
+    }
+
+
+}
+
+private suspend fun getPostData(number : Int) : Post?{
+
+    val retrofitInstance = RetrofitInstance.getInstance().create(MyApi::class.java)
+    val response = retrofitInstance.getPostNumber(number)
+    return if(response.isSuccessful) response.body() else null
 }
 
 data class Post(
-    val userId : Int,
-    val id : Int,
-    val title : String,
-    val body : String
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
 )
 
-object RetrofitInstance{
+object RetrofitInstance {
     val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
     val client = Retrofit
@@ -127,13 +167,20 @@ object RetrofitInstance{
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    fun getInstance() : Retrofit { return client}
+    fun getInstance(): Retrofit {
+        return client
+    }
 }
 
-interface MyApi{
+interface MyApi {
 
     @GET("posts/1")
-    suspend fun getPost1() : Response<Post>
+    suspend fun getPost1(): Response<Post>
+
+    @GET("posts/{number}")
+    suspend fun getPostNumber(
+        @Path("number") number: Int
+    ): Response<Post>
 }
 
 
