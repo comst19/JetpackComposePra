cfe0c29248c664bea6c6f17efa03a6b7cbcd22fb
comst19 <comst19@naver.com>
2024-02-25 05:42:25 +0900

    인프런 : Theme - shape

---
+++ MyThemeTest4/app/src/main/java/com/comst/mythemetest4/ui/theme/Shapes.kt
@@ -0,0 +1,14 @@
+package com.comst.mythemetest4.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+
+
+    extraLarge = RoundedCornerShape(100.dp),
+    large = RoundedCornerShape(10.dp),
+    medium = RoundedCornerShape(1.dp)
+
+)
