29a7466b972d7dda9a9f620aa0e98e57598b9c59
comst19 <comst19@naver.com>
2024-02-26 21:05:21 +0900

    인프런 : CoupangEx

--- CoupangEx/app/src/main/java/com/comst/coupangex/MainActivity.kt
+++ CoupangEx/app/src/main/java/com/comst/coupangex/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
@@ -69,11 +70,16 @@ fun CoupangEx() {
         color = MaterialTheme.colorScheme.background
     ) {
 
-        Column() {
+        val scrollState = rememberScrollState()
+
+        Column(
+            modifier = Modifier.verticalScroll(scrollState)
+        ) {
             TopLageArea()
             TopSearchBarArea()
             TopBanner()
             CategoryList()
+            CenterBannerArea()
         }
     }
 }
@@ -239,6 +245,21 @@ fun CategoryList() {
     }
 }
 
+@Composable
+fun CenterBannerArea() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(500.dp)
+            .padding(20.dp)
+            .background(Color.LightGray),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "배너영역", fontSize = 30.sp, fontWeight = FontWeight.ExtraBold)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -247,5 +268,6 @@ fun GreetingPreview() {
         TopSearchBarArea()
         TopBanner()
         CategoryList()
+        CenterBannerArea()
     }
 }
