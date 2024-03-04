99b41cfc650e3368278539b2f8689a1ac06c0cac
comst19 <comst19@naver.com>
2024-03-04 23:05:42 +0900

    인프런 - FinanceApp : Header

--- FinanceApp/app/src/main/java/com/comst/financeapp/screen/MainScreen.kt
+++ FinanceApp/app/src/main/java/com/comst/financeapp/screen/MainScreen.kt
@@ -1,7 +1,16 @@
 package com.comst.financeapp.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -13,11 +22,37 @@ import com.guru.fontawesomecomposelib.FaIcons
 @Composable
 fun MainScreen() {
 
-    FaIcon(
-        faIcon = FaIcons.ChartBarRegular,
-        size = 50.dp,
-        tint = Color.Red
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(Color.Black)
+    ) {
+
+    }
+
+    Header()
+}
+
+@Composable
+fun Header() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        FaIcon(
+            faIcon = FaIcons.ArrowLeft,
+            tint = Color.White,
+            modifier = Modifier.padding(20.dp)
+        )
+        FaIcon(
+            faIcon = FaIcons.Plus,
+            tint = Color.White,
+            modifier = Modifier.padding(20.dp)
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
