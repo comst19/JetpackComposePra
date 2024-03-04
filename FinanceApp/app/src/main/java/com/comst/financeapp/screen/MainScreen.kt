0ac2295918dfed39aba3b11b35c9efd82f581c04
comst19 <comst19@naver.com>
2024-03-04 23:17:36 +0900

    인프런 - FinanceApp : TopMenu

--- FinanceApp/app/src/main/java/com/comst/financeapp/screen/MainScreen.kt
+++ FinanceApp/app/src/main/java/com/comst/financeapp/screen/MainScreen.kt
@@ -2,14 +2,19 @@ package com.comst.financeapp.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,10 +33,12 @@ fun MainScreen() {
             .verticalScroll(rememberScrollState())
             .background(Color.Black)
     ) {
-
+        Header()
+        TopMenu()
+        TopMenuBottom()
     }
 
-    Header()
+
 }
 
 @Composable
@@ -52,6 +59,61 @@ fun Header() {
             modifier = Modifier.padding(20.dp)
         )
 
+    }
+}
+
+@Composable
+fun TopMenu() {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp)
+    ) {
+
+        Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+            Text(text = "자산", color = Color.Gray, fontSize = 16.sp)
+        }
+
+        Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+            Text(text = "소비·수입", color = Color.Gray, fontSize = 16.sp)
+        }
+
+        Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+            Text(text = "연말정산", color = Color.Gray, fontSize = 16.sp)
+        }
+    }
+}
+
+@Composable
+fun TopMenuBottom() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp)
+    ) {
+
+        Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+
+
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(2.dp)
+                .background(Color.White)
+                .padding(top = 15.dp, start = 5.dp, end = 5.dp),
+            contentAlignment = Alignment.Center
+        ) {
+
+
+        }
+
+        Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+
+
+        }
     }
 }
 
