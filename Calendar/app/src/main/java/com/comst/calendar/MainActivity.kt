3ff6e9631c34b0235960b6929e5bb9492773756a
comst19 <comst19@naver.com>
2024-03-10 13:25:58 +0900

    인프런 - Calender 2 - 달력 요일 만들기

--- Calendar/app/src/main/java/com/comst/calendar/MainActivity.kt
+++ Calendar/app/src/main/java/com/comst/calendar/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,10 +15,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.comst.calendar.ui.theme.CalendarTheme
 import com.guru.fontawesomecomposelib.FaIcon
 import com.guru.fontawesomecomposelib.FaIcons
@@ -34,6 +37,7 @@ class MainActivity : ComponentActivity() {
                         .background(Color.White)
                 ) {
                     CalendarHeader()
+                    CalendarDayNames()
                 }
             }
         }
@@ -71,5 +75,22 @@ fun CalendarHeader() {
             tint = Color.Gray,
             modifier = Modifier.padding(end = 10.dp)
         )
+    }
+}
+
+@Composable
+fun CalendarDayNames() {
+
+    val nameList = listOf("일", "월", "화", "수", "목", "금", "토")
+
+    Row() {
+        nameList.forEach {
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = it, fontSize = 14.sp)
+            }
+        }
     }
 }
