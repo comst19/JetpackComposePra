d98503fd5df41a4a0a26a8e7849bb3912c9e5f7c
comst19 <comst19@naver.com>
2024-03-11 16:35:19 +0900

    인프런 - Calender 8 - LazyRow

--- Calendar/app/src/main/java/com/comst/calendar/MainActivity.kt
+++ Calendar/app/src/main/java/com/comst/calendar/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -207,7 +208,11 @@ fun CalendarLazeList() {
     }
 
     if (isScrolling.value){
-
+        LazyRow(modifier = Modifier.fillMaxSize().height(60.dp)){
+            items(spendingData.size){
+                
+            }
+        }
     }else{
         CalendarDayNames()
         CalendarDayList()
